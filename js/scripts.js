989f0fe2cb7cb968caa630381aedabfa87cf40ba
guilherme.petry <guilhermepetryrs@gmail.com>
2021-10-03 17:48:44 -0300

    reverted

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
 const pad = document.body.getElementsByClassName("sketch-pad")[0];
+const buttons = document.body.getElementsByClassName("buttons")[0];
 
 function makeGrid(size) {
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
-    square.className = "square";
+    square.className = "3";
+    pad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    pad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     pad.appendChild(square);
   }
 }
-makeGrid(16 * 16);
+makeGrid(16);
 
-function paint() {
 
+function paint(e) {
+  if (e.target.className !== "sketch-pad"){
+    let blackLevel = parseInt(e.target.className);
+    blackLevel++;
+    if (blackLevel < 9){
+      e.target.style.backgroundColor = `rgba(0,0,0,0.${blackLevel})`;
+    }
+    e.target.className = `${blackLevel}`;
+  }
 }
 
-window.addEventListener("hover", (e) => {
-  console.log(e);
-});
+function isClicked(e){
+  if (e.target.className === "clicked"){
+    e.target.classList.remove("clicked");
+    return true;
+  }
+  e.target.className = "clicked";
+  return false;
+}
+
+pad.addEventListener("mouseover", paint);
